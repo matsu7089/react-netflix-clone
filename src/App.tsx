0b40fcdc89817b07e1590fb4b4e367cee6814d4f
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2021-01-24 00:31:53 +0900

    update: App.tsxを更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import { requests } from './api/tmdb'
+import { Nav } from './components/Nav'
+import { Banner } from './components/Banner'
+import { Row } from './components/Row'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <>
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
+    </>
+  )
 }
 
 export default App
